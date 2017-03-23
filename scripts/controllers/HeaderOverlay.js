7b501d0c1c707837b5c0840315f9ff73ad4ee8a2
George Zhang <gzhang@squarespace.com>
2017-03-23 16:18:39 -0400

    Adding Header--overlay class so tweaks show up for header index gallery overlay, fixing spacing in index gallery content

--- scripts/controllers/HeaderOverlay.js
+++ scripts/controllers/HeaderOverlay.js
@@ -1,9 +1,11 @@
+import { Tweak } from '@squarespace/core';
 import { resizeEnd } from '../util';
 import { indexEditEvents } from '../constants';
 
 const overlayClassname = 'Header--overlay';
 const indexContentSelector = '.Index-page--has-image:first-child .Index-page-content';
 const introContentSelector = '.Intro--has-image .Intro-content';
+// const indexGalleryContentSelector = '.Index-gallery-item-content';
 
 /**
  * Manages overlay header in two ways:
@@ -39,6 +41,17 @@ function HeaderOverlay(element) {
    * classname and applies offset where appropriate.
    */
   const sync = () => {
+    const overlayOnIndexGallery = Tweak.getValue('tweak-header-bottom-overlay-on-index-gallery') === 'true';
+    const isOverIndexGallery = header.classList.contains('Header--index-gallery');
+
+    if (overlayOnIndexGallery && isOverIndexGallery) {
+      header.classList.add(overlayClassname);
+      // indexGalleryContents.forEach((content) => {
+      //   applyOffset(content);
+      // });
+      return;
+    }
+
     const indexPageContent = element.querySelector(indexContentSelector);
     const introContent = element.querySelector(introContentSelector);
     const offsetElement = indexPageContent || introContent;
